ac84b03f934a3187fc5c27bece4ec79f935f663b
Radiotechniman <spamisgood4you@gmail.com>
2016-11-13 20:37:39 +0100

    success !

--- drivers/NAS-DS01Z/driver.js
+++ drivers/NAS-DS01Z/driver.js
@@ -11,8 +11,7 @@ module.exports = new ZwaveDriver( path.basename(__dirname), {
 			'command_report': 'SENSOR_BINARY_REPORT',
 			'command_report_parser': report => report['Sensor Value'] === 'detected an event'
 		},
-		
-		'measure_battery': {
+		'alarm_battery': {
 			'command_class': 'COMMAND_CLASS_BATTERY',
 			'command_get': 'BATTERY_GET',
 			'command_report': 'BATTERY_REPORT',
@@ -22,22 +21,33 @@ module.exports = new ZwaveDriver( path.basename(__dirname), {
 				return report['Battery Level (Raw)'][0];
 			}
 		},
+		'measure_battery': {
+			'command_class': 'COMMAND_CLASS_BATTERY',
+			'command_get': 'BATTERY_GET',
+			'command_report': 'BATTERY_REPORT',
+			'command_report_parser': function (report) {
+				if(report['Battery Level'] === "battery low warning") {
+					 return 1;
+					} else {
+					 return report['Battery Level (Raw)'][0];
+				}
+ 			}
+		}
 	},
-	
 	settings: {
 		"off_delay": {
 			"index": 1,
 			"size": 1,
 			"parser": function( input ) {
-                return new Buffer([ parseInt(input) ]);
-                  }
+				return new Buffer([ parseInt(input) ]);
+			}
 		},
 		"basic_level_set": {
 			"index": 2,
 			"size": 2,
 			"parser": function( input ) {
-                return new Buffer([ parseInt(input) ]);
-                  }
+				return new Buffer([ parseInt(input) ]);
+			}
 		},
 	}
 });
